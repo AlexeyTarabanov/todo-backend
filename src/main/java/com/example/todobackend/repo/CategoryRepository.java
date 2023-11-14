672054e3591deece99d1885555c94a408bc8cf4c
Alexey Tarabanov <tarabanov@bk.ru>
2023-11-14 23:11:07 +0300

    Добавление запроса в Repository

--- src/main/java/com/example/todobackend/repo/CategoryRepository.java
+++ src/main/java/com/example/todobackend/repo/CategoryRepository.java
@@ -4,7 +4,10 @@ import com.example.todobackend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // принцип ООП: абстракция-реализация - здесь описываем все доступные способы доступа
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByUserEmailOrderByTitleAsc(String email);
 }
